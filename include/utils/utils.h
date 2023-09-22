b3e010e7e40dc6f6ade36dfdb1a452f85b7ccc32
Wartuu <88190400+Wartuu@users.noreply.github.com>
2023-09-22 17:56:42 +0200

    commit fix

--- include/utils/utils.h
+++
@@ -1,9 +0,0 @@
-#include <fstream>
-#include <string>
-#include <iostream>
-#include <vector>
-
-#pragma once
-
-std::string readText(const char* filename);
-unsigned char* loadFile(const char* filename, int* size);
--- src/utils/error.cpp
+++
@@ -1,5 +0,0 @@
-#include <utils/error.h>
-
-void util::throwError(ERROR_TYPE error) {
-	std::exit(abs(error)*-1);
-}
